f4c4f92175d3b98e55151fcf668a68f659bd222d
Karim <eng.karim.mousa.89@gmail.com>
2026-01-15 15:03:58 +0300

    fix retries count issue

--- AmanatInspection/AmanatInspection/Networking/NetworkManager.swift
+++ AmanatInspection/AmanatInspection/Networking/NetworkManager.swift
@@ -6,82 +6,22 @@
 //
 import Foundation
 
-struct NetworkManager {
-    /// To catch the Error
-    /// catch is CancellationError {
-    ///     print("Request was cancelled")
-    /// } catch let error as NetworkError {
-    ///     print("Network error: \(error)")
-    /// }
-    func requestData(_ request: NetworkRequest) async throws -> (Int, Data, HTTPURLResponse) {
-        var attempt = 0
+protocol NetworkManager{
+    func requestData(_ request: NetworkRequest) async throws -> Data
+    func requestResult(_ request: NetworkRequest) async -> Result<Data, Error>
+    func requestJson<T: Decodable>(_ request: NetworkRequest) async throws -> T
+    func requestJsonResult<T: Decodable>(_ request: NetworkRequest) async -> Result<T, Error>
+}
 
-        while true {
-            // Check for cancellation at the start of each loop
-            try Task.checkCancellation()
-            do {
-                let result =  try await performData(request)
-                return (attempt, result.0, result.1)
-            } catch let error {
-                // transport Error, no reposne or data
-                // not valid response HTTPURLResponse, no response or data
-                // http status outside 200..<299, response exist and data may exist
-                // can't construct the Auth header
-                if case NetworkError.authGenerationFailure(_) = error{
-                    throw error
-                }
-                
-                var data: Data?
-                var response: HTTPURLResponse?
-                
-                if case NetworkError.invalidResponseStatus(let response1, let data1) = error{
-                    response = response1
-                    data = data1
-                }
-                
-                if let trigger = request.authorizationGroup?.refreshTrigger,
-                   let coordinator = request.authorizationGroup?.refreshCoordinator,
-                   trigger.shouldRefresh(
-                        error: error,
-                        response: response,
-                        data: data
-                   ) {
-                    try await coordinator.refresh()
-                    
-                    attempt += 1
-                    continue // retry original request
-                }
-                
-                attempt += 1
-
-                guard let policy = request.retryPolicy else {
-                    throw error
-                }
-
-                if let decision = policy.shouldRetry(
-                    attempt: attempt,
-                    error: error,
-                    response: response,
-                    data: data,
-                    considerContent : .nonContentBasedOnly
-                ) {
-                    switch decision {
-                    case .retryImmediately:
-                        continue
-                    case .retry(let delay):
-                        try await Task.cancellableSleep(seconds: delay)
-                        continue
-                    }
-                } else {
-                    throw error
-                }
-            }
-        }
+struct NetworkManagerImp: NetworkManager {
+    func requestData(_ request: NetworkRequest) async throws -> Data {
+        var attempts = 0
+        return try await self.requestData(request, executions: &attempts).0
     }
     
     func requestResult(_ request: NetworkRequest) async -> Result<Data, Error> {
         do {
-            let (_, data, _) = try await self.requestData(request)
+            let (data) = try await self.requestData(request)
             return .success(data)
         } catch {
             return .failure(error)
@@ -89,26 +29,23 @@ struct NetworkManager {
     }
     
     func requestJson<T: Decodable>(_ request: NetworkRequest) async throws -> T{
-        var attempt = 0
+        var executions = 0
         
         while true {
             try Task.checkCancellation()
-            let (currentAttempt ,data, response) = try await self.requestData(request)
-            attempt = currentAttempt
+            let (data, response) = try await self.requestData(request, executions: &executions)
             do {
                 let decoder = JSONDecoder()
                 let decodedResponse = try decoder.decode(T.self, from: data)
                 return decodedResponse
             } catch (let error) {
                 // decoding error, data and response exist
-                 attempt += 1
-                
                 guard let policy = request.retryPolicy else {
                     throw error
                 }
                 
                 if let decision = policy.shouldRetry(
-                    attempt: attempt,
+                    attempt: executions,
                     error: NetworkError.decodingFailure(error: error),
                     response: response,
                     data: data,
@@ -139,6 +76,74 @@ struct NetworkManager {
 }
 
 private extension NetworkManager {
+    /// To catch the Error
+    /// catch is CancellationError {
+    ///     print("Request was cancelled")
+    /// } catch let error as NetworkError {
+    ///     print("Network error: \(error)")
+    /// }
+    func requestData(_ request: NetworkRequest, executions: inout Int) async throws -> (Data, HTTPURLResponse) {
+        while true {
+            // Check for cancellation at the start of each loop
+            try Task.checkCancellation()
+            do {
+                // attempts counts actual network executions (performData calls)
+                executions += 1
+                let result =  try await performData(request)
+                return (result.0, result.1)
+            } catch let error {
+                // transport Error, no reposne or data
+                // not valid response HTTPURLResponse, no response or data
+                // http status outside 200..<299, response exist and data may exist
+                // can't construct the Auth header
+                if case NetworkError.authGenerationFailure(_) = error{
+                    throw error
+                }
+                
+                var data: Data?
+                var response: HTTPURLResponse?
+                
+                if case NetworkError.invalidResponseStatus(let response1, let data1) = error{
+                    response = response1
+                    data = data1
+                }
+                
+                if let trigger = request.authorizationGroup?.refreshTrigger,
+                   let coordinator = request.authorizationGroup?.refreshCoordinator,
+                   trigger.shouldRefresh(
+                        error: error,
+                        response: response,
+                        data: data
+                   ) {
+                    try await coordinator.refresh()
+                    continue // retry original request
+                }
+
+                guard let policy = request.retryPolicy else {
+                    throw error
+                }
+
+                if let decision = policy.shouldRetry(
+                    attempt: executions,
+                    error: error,
+                    response: response,
+                    data: data,
+                    considerContent : .nonContentBasedOnly
+                ) {
+                    switch decision {
+                    case .retryImmediately:
+                        continue
+                    case .retry(let delay):
+                        try await Task.cancellableSleep(seconds: delay)
+                        continue
+                    }
+                } else {
+                    throw error
+                }
+            }
+        }
+    }
+    
     func performData(_ request: NetworkRequest) async throws(NetworkError) -> (Data, HTTPURLResponse) {
         guard var components = URLComponents(string: request.urlString) else {
             throw NetworkError.invalidURL
